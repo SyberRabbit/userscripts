0bebfe4260e26664264f2a405692ee85aeb50db7
Cat <62778100+SyberRabbit@users.noreply.github.com>
2026-09-23 10:54:04 +0800

    Update metadata in subscribe.user.sub.js

--- subscribe.user.sub.js
+++ subscribe.user.sub.js
@@ -3,6 +3,7 @@
 // @description  Sola 自用脚本合集：DeepSeek / 知乎跟作者更新，其余为本仓库脚本
 // @version      0.3.1
 // @author       Sola
+// @updateURL    https://raw.githubusercontent.com/SyberRabbit/userscripts/main/subscribe.user.sub.js
 // @namespace    https://github.com/SyberRabbit/userscripts
 // @homepage     https://github.com/SyberRabbit/userscripts
 // @license      MIT
