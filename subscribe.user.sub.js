a42a2753606f7416659c5d2f4590a59084847984
Cat <62778100+SyberRabbit@users.noreply.github.com>
2026-09-23 10:50:26 +0800

    Bump version to 0.3.1 in user script

--- subscribe.user.sub.js
+++ subscribe.user.sub.js
@@ -1,7 +1,7 @@
 // ==UserSubscribe==
 // @name         Sola 用户脚本订阅
 // @description  Sola 自用脚本合集：DeepSeek / 知乎跟作者更新，其余为本仓库脚本
-// @version      0.3.0
+// @version      0.3.1
 // @author       Sola
 // @namespace    https://github.com/SyberRabbit/userscripts
 // @homepage     https://github.com/SyberRabbit/userscripts
@@ -10,7 +10,6 @@
 // @connect      localhost
 // @scriptUrl    https://update.greasyfork.org/scripts/578512/DeepSeek%20%E8%B6%85%E7%BA%A7%E5%B7%A5%E5%85%B7%E7%AE%B1.user.js
 // @scriptUrl    https://update.greasyfork.org/scripts/21659/%E7%9F%A5%E4%B9%8E%E6%8E%92%E7%89%88%E4%BC%98%E5%8C%96.user.js
-// @scriptUrl    https://cdn.jsdelivr.net/gh/SyberRabbit/userscripts@main/scripts/linux-do-prevent-esc.user.js
 // @scriptUrl    https://cdn.jsdelivr.net/gh/SyberRabbit/userscripts@main/scripts/clash-connection-monitor.user.js
 // @scriptUrl    https://cdn.jsdelivr.net/gh/SyberRabbit/userscripts@main/scripts/hxc-pure-reverse.user.js
 // ==/UserSubscribe==
