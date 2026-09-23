35067646e236073c455b722947ead2dff626415e
Cat <62778100+SyberRabbit@users.noreply.github.com>
2026-09-23 12:46:12 +0800

    Update subscribe.user.sub.js

--- subscribe.user.sub.js
+++ subscribe.user.sub.js
@@ -1,7 +1,7 @@
 // ==UserSubscribe==
 // @name         Sola 用户脚本订阅
 // @description  Sola 自用脚本合集：DeepSeek / 知乎跟作者更新，其余为本仓库脚本
-// @version      0.3.2
+// @version      0.3.3
 // @author       Sola
 // @namespace    https://github.com/SyberRabbit/userscripts
 // @homepage     https://github.com/SyberRabbit/userscripts
